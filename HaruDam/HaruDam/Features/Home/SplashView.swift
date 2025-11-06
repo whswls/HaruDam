4b44465d2cb9a9e530318de435a9dbe9ac921f8e
EunJinCHO <till5941@gmail.com>
2025-11-06 18:15:08 +0900

    chore: 기본 폴더 구조 생성

---
+++ HaruDam/HaruDam/Features/Home/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  HaruDam
+//
+//  Created by 존진 on 11/6/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
---
+++ HaruDam/HaruDam/HaruDamApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaruDamApp.swift
+//  HaruDam
+//
+//  Created by 존진 on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct HaruDamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
