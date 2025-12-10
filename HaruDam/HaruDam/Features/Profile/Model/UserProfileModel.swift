3d3a632903bfa838c4504e5df42e417f76881504
EunJinCHO <till5941@gmail.com>
2025-12-10 16:49:10 +0900

    feat: UserProfileModel 도메인 모델 추가
    - 서버 및 CoreData와 분리된 앱 내부 전용 도메인 모델 정의

---
+++ HaruDam/HaruDam/Features/Profile/Model/UserProfileModel.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfile.swift
+//  HaruDam
+//
+//  Created by 존진 on 12/10/25.
+//  Copyright © 2025 kr.co.HaruDam. All rights reserved.
+//
+
+import Foundation
+
+struct UserProfileModel {
+    let id: String
+    let email: String
+    let nickname: String
+    let profileImageURL: URL?
+    let createdAt: Date?
+}
+
