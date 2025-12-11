0e898eca88bd1f61cb2460879b85c55a09116a0c
EunJinCHO <till5941@gmail.com>
2025-12-11 23:07:11 +0900

    feat: Keychain 저장 기능 구현
    - 문자열 값을 UTF-8 Data로 변환하여 Keychain에 안전하게 저장
    - 기존 항목 삭제 후 새 값 저장하도록 로직 구성

--- HaruDam/HaruDam/Shared/KeychainManager.swift
+++ HaruDam/HaruDam/Shared/KeychainManager.swift
@@ -9,6 +9,34 @@
 import Foundation
 import Security
 
+enum KeychainKey: String{
+    case userId
+}
+
 final class KeychainManager {
+    static let shared = KeychainManager()
+    private init() {}
     
+    @discardableResult
+    func save(_ value: String, for key: KeychainKey) -> Bool {
+        guard let data = value.data(using: .utf8) else { return false }
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key.rawValue
+        ]
+        
+        // 기존 값 삭제
+        SecItemDelete(query as CFDictionary)
+        
+        // 새 값 추가
+        let attributes: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key.rawValue,
+            kSecValueData as String: data
+        ]
+        
+        let status = SecItemAdd(attributes as CFDictionary, nil)
+        return status == errSecSuccess
+    }
 }
