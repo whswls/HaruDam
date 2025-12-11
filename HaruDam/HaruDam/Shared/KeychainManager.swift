e71359c2acc181a591ddd6e1b6900813dfecd5ee
EunJinCHO <till5941@gmail.com>
2025-12-11 22:23:12 +0900

    feat: KeychainManager 파일 생성 및 기본 구조 추가
    - Keychain 기반 사용자 데이터 저장 기능을 위한 파일 생성

---
+++ HaruDam/HaruDam/Shared/KeychainManager.swift
@@ -0,0 +1,14 @@
+//
+//  KeychainManager.swift
+//  HaruDam
+//
+//  Created by 존진 on 12/11/25.
+//  Copyright © 2025 kr.co.HaruDam. All rights reserved.
+//
+
+import Foundation
+import Security
+
+final class KeychainManager {
+    
+}
