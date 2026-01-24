93b76587235bd4e4a3e64bdfc1d8f8b54467e418
EunJinCHO <till5941@gmail.com>
2026-01-24 19:44:36 +0900

    feat: 주간 감정 요약 카드 모델 추가
    - 감정 기록 데이터를 기반으로 한 주간 감정 요약 구조 정의

---
+++ HaruDam/HaruDam/Features/Home/Model/WeeklyEmotionSummary.swift
@@ -0,0 +1,16 @@
+//
+//  WeeklyEmotionSummary.swift
+//  HaruDam
+//
+//  Created by 존진 on 1/23/26.
+//  Copyright © 2026 kr.co.HaruDam. All rights reserved.
+//
+
+import Foundation
+
+struct WeeklyEmotionSummary: Equatable {
+    let emoji: String
+    let title: String
+    let description: String
+    let recordedDays: Int
+}
